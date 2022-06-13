6a5a26438bef3fd91961878807ff12f4043519db
Syam Sundar <101164228+SS-TechCodes@users.noreply.github.com>
2022-06-13 12:32:05 +0530

    firstCommit
    firstCommit

---
+++ src/main/java/com/example/demo/Students/Studentsrepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.Students;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import com.example.demo.Students.*;
+
+public interface Studentsrepository extends MongoRepository <Students,String>{
+	Students findBystudentNumber (String studentNumber);
+
+}
